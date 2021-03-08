64f36244ca9cae640cea98e5340a317bc288e6a5
Felipe Borges <lipex360@gmail.com>
2021-03-07 21:58:32 -0300

    Graphql added

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,25 +1,48 @@
-export default function Page({ title, text }) {
-  return (
-    <>
-      <h1>{title}</h1>
-      <p>{text}</p>
-    </>
-  )
+import { GetStaticProps } from 'next'
+import client from 'graphql/client'
+
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
+import { useRouter } from 'next/dist/client/router'
+import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
+
+export default function Page({ heading, body }: PageTemplateProps) {
+  const router = useRouter()
+
+  // Return load
+  if (router.isFallback) return null
+
+  return <PageTemplate heading={heading} body={body} />
 }
 
+// Generate URL
 export async function getStaticPaths() {
-  const paths = [{
-    params: { slug: 'about' }
-  }]
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
 
   return { paths, fallback: true }
 }
 
-export const getStaticProps = async() => {
+// Get data
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+
   return {
     props: {
-      title: 'About Page',
-      text: 'About page Text'
+      heading: page.heading,
+      body: page.body.html
     }
   }
 }
+
+// getStaticPaths: Generate URL (build)
+// getStaticProps: Get data (props da página - build)
+// getServerSideProps: Get data (runtime)
